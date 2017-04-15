a8adb18479c06c273129b31865370c5acb6cd7fb
javamoving <623943190@qq.com>
2017-04-15 15:36:17 +0800

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -76,7 +76,7 @@ public class HelloWorld extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 	}
 
